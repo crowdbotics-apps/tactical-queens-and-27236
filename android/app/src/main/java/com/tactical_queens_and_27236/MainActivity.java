066cf3de270c1eec065c5bd89da19821e3535cd9
Crowdbotics <team@crowdbotics.com>
2021-05-22 07:22:01 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tactical_queens_and_27236/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tactical_queens_and_27236;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "tactical_queens_and_27236";
+  }
+}
---
+++ backend/tactical_queens_and_27236/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tactical_queens_and_27236 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tactical_queens_and_27236.settings')
+
+application = get_wsgi_application()
